1c6ef61827c14f4cd9de3b4471bea023a6a1106c
marthaberner <marthaberner.mb@gmail.com>
2016-01-21 13:33:40 -0700

    basic solution

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var validate = require('../lib/validations');
 
-router.get('/', function(req, res, next) {  
+router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/', function (req, res, next) {
+  var errors = [];
+  errors.push(validate.nameIsNotBlank(req.body.name));
+  errors.push(validate.emailIsValid(req.body.email));
+  errors.push(validate.phoneIsValid(req.body.phone));
+  errors = validate.validate(errors);
+  if(errors.length){
+    res.render('index', {errors: errors})
+  } else {
+    res.send('You are so valid!');
+  }
+})
 
 module.exports = router;
--- test/test_user_input_validatons.js
+++ test/test_user_input_validatons.js
@@ -14,7 +14,7 @@ describe('User Validations', function () {
   });
   describe('Email is Valid', function () {
     xit('should not be blank', function (done) {
-      validate.emailIsNotBlank.should.be.a('function');
+      validate.emailIsValid.should.be.a('function');
       assert.equal(validate.emailIsNotBlank('    '), 'Email is invalid')
       assert.equal(validate.emailIsNotBlank('me@me.com'), true)
       done();
@@ -22,7 +22,7 @@ describe('User Validations', function () {
   });
   describe('Phone Number is Valid', function () {
     xit('should not be blank', function (done) {
-      validate.phoneNumberIsValid.should.be.a('function');
+      validate.phoneIsValid.should.be.a('function');
       assert.equal(validate.phoneNumberIsValid('  '), 'Phone number is invalid');
       done();
     });
